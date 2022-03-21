659fd3e7257351e62e6f24ef7d261f62da7c64b5
Ben Smith <bh2smith@gmail.com>
2022-03-21 16:17:08 +0100

    add templating for multinetwork subgraphs

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,38 @@
+import { Address, log } from "@graphprotocol/graph-ts";
 import { Transfer } from "../generated/CowProtocolToken/CowProtocolToken";
 
-export function handleTransfer(event: Transfer): void {}
+const NON_CIRCULATING_MAINNET = [
+  // CoW DAO Safe
+  Address.fromString("0xcA771eda0c70aA7d053aB1B25004559B918FE662"),
+  // Gchain Vested tokens (vCOW token address)
+  Address.fromString("0xc20C9C13E853fc64d054b73fF21d3636B2d97eaB"),
+  // TODO - add GnosisDAO Vesting contract Mainnet
+];
+
+const NON_CIRCULATING_GCHAIN = [
+  // CoW DAO Safe
+  Address.fromString("0xcA771eda0c70aA7d053aB1B25004559B918FE662"),
+  // Gchain Vested tokens (vCOW token address)
+  Address.fromString("0xc20C9C13E853fc64d054b73fF21d3636B2d97eaB"),
+  // TODO - add GnosisDAO Vesting contract Gchain
+];
+
+export function handleTransfer(
+  event: Transfer,
+  nonCirculating: Address[]
+): void {
+  log.info("Received Transfer Event at tx {}", [
+    event.transaction.hash.toHex(),
+  ]);
+  log.info("Non circulating supply list has length {}", [
+    nonCirculating.length.toString(),
+  ]);
+}
+
+export function handleMainnetTransfer(event: Transfer): void {
+  handleTransfer(event, NON_CIRCULATING_MAINNET);
+}
+
+export function handleGchainTransfer(event: Transfer): void {
+  handleTransfer(event, NON_CIRCULATING_GCHAIN);
+}
